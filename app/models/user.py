bdb2411e21045d5c7df276b88b1916469112cae0
Martin Schneider <martin.schne@protonmail.com>
2025-05-19 13:55:37 +0200

    add password validation to UserCreate model

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime, timezone
 from typing import List
 
-from pydantic import EmailStr, BaseModel
+from pydantic import EmailStr, BaseModel, field_validator
 from sqlmodel import SQLModel, Field, Relationship
 
 
@@ -21,6 +21,19 @@ class User(SQLModel, table=True):
 class UserCreate(BaseModel):
     username: EmailStr
     password: str
+
+    @field_validator("password")
+    def check_password(cls, value):
+        value = str(value)
+        if len(value) < 8:
+            raise ValueError("Password must have at least 8 characters")
+        if not any(c.isupper() for c in value):
+            raise ValueError("Password must have at least one uppercase letter")
+        if not any(c.islower() for c in value):
+            raise ValueError("Password must have at least one lowercase letter")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must have at least one digit")
+        return value
 
 
 class UserFlags(BaseModel):
